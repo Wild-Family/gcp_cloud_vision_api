1b919f84c61be855e020b4d1b6992c8d88085c81
Yusuke Suzuki <cs16047@s.inf.shizuoka.ac.jp>
2018-03-08 21:40:12 +0900

    閾値の設定

--- gcp_cloud_vision_api.py
+++ gcp_cloud_vision_api.py
@@ -6,20 +6,34 @@ from google.cloud.vision import types
 from PIL import Image, ImageDraw
 
 
-def check_face_loc(left_eye,right_eye,nose_tip):
-    print(nose_tip.x)
-    if(nose_tip.x < 1024*4/7):
-        print("もうちょい右やで")    
-        return "move right"
-    if(nose_tip.x > 1024*4/7):
-        print("もうちょい左やで")
-        return "move left"
-    if(nose_tip.y < 768*4/7):
-        print("もうちょい下(後ろに下がって)")
-        return "get back"
-    if(nose_tip.y > 768*4/7):
-        print("もうちょい上(前に出て)")
+#TODO face_boxを連想配列
+def check_face_loc(face_box,left_eye,right_eye,nose_tip):
+    if((face_box[0][0]-face_box[1][0])*(face_box[1][1]-face_box[2][1]) < 200 * 200):#ここ調整
+        print("もう少し近づいて")
         return "move forward"
+    if(face_box[0][0] > 1024*1/2):
+        return "right" #被写体は右に
+    if(face_box[1][0] < 1024*1/2):
+        return "left" #被写体は左に
+    if(face_box[0][1] > 768*1/2):
+        return "forward" #顔はもう少し上に
+    if(face_box[3][1] < 768*1/2):
+        return "back" #顔はもう少し下に
+    
+    
+    # print(nose_tip.x)
+    # if(nose_tip.x < 1024*4/7):
+    #     print("もうちょい右やで")    
+    #     return "move right"
+    # if(nose_tip.x > 1024*4/7):
+    #     print("もうちょい左やで")
+    #     return "move left"
+    # if(nose_tip.y < 768*4/7):
+    #     print("もうちょい下(後ろに下がって)")
+    #     return "get back"
+    # if(nose_tip.y > 768*4/7):
+    #     print("もうちょい上(前に出て)")
+    #     return "move forward"
     return "ok"
 
 def detect_face(face_file, max_results=4):
@@ -33,14 +47,19 @@ def highlight_faces(image, faces):
     for face in faces:
         left_eye =  face.landmarks[0].position
         right_eye = face.landmarks[1].position
-        nose_tip =  face.landmarks[7].position 
+        nose_tip =  face.landmarks[7].position
+
+        box = [(vertex.x, vertex.y) for vertex in face.bounding_poly.vertices]#(左上、右上、右下、左下)
         #print(faces)
         #print(face.landmarks[0].position)#左目
         #print(face.landmarks[1].position)#右目
     #print(left_eye)
     #print(right_eye)
     #print(nose_tip)
-    return check_face_loc(left_eye,right_eye,nose_tip)
+    print(box)
+    
+
+    return check_face_loc(box,left_eye,right_eye,nose_tip)
 
 def main(input_filename,max_results):
     with open(input_filename, 'rb') as image:
@@ -50,5 +69,6 @@ def main(input_filename,max_results):
 
 
 if __name__ == '__main__':
-    check_face_loc_result = main("img/test.JPG",4)
+    check_face_loc_result = main("img/tes.JPG",4)
     print(check_face_loc_result)
+
